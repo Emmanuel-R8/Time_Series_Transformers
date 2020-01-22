792589b44ff7afca7f851bb2276958a21dbd3477
Xingjian Shi <xshiab@connect.ust.hk>
2020-01-21 23:30:13 -0800

    update

--- pytorch/run_wt103_base.sh
+++ pytorch/run_wt103_base.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == 'train' ]]; then
     echo 'Run training...'
-    python train.py \
+    python3 train.py \
         --cuda \
         --data ../data/wikitext-103/ \
         --dataset wt103 \
@@ -27,7 +27,7 @@ if [[ $1 == 'train' ]]; then
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
-    python eval.py \
+    python3 eval.py \
         --cuda \
         --data ../data/wikitext-103/ \
         --dataset wt103 \
