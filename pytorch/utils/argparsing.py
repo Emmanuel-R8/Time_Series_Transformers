da5f580d5631689cf4d8e7b830e200effe023248
TevenLeScao <teven.lescao@gmail.com>
2020-04-28 13:02:58 +0200

    fixed integration arg

--- pytorch/utils/argparsing.py
+++ pytorch/utils/argparsing.py
@@ -138,7 +138,7 @@ parser.add_argument('--wandb', action='store_true',
                     help='Use weights and biases logging.')
 parser.add_argument('--expand', type=str, default=None, help='Expand models throughout training.',
                     choices=["repeat", "reinit", "repeat_bottom", "reinit_bottom", "zero_expectancy"])
-parser.add_argument('--integration', type=str, default=None, help='Learning tricks post-expansion',
+parser.add_argument('--integration', type=str, default="", help='Learning tricks post-expansion',
                     choices=["freeze", "reverse_distil_full", "reverse_distil_partial"])
 parser.add_argument('--integration_length', type=int, default=0,
                     help='Number of batches for reverse distillation or freezing')
