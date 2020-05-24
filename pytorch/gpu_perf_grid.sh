e7c2308ca2573906c4d18a7a99c28e19d31dbe44
TevenLeScao <teven.lescao@gmail.com>
2020-05-25 01:19:02 +0200

    updated grid script

--- pytorch/gpu_perf_grid.sh
+++ pytorch/gpu_perf_grid.sh
@@ -1,13 +1,13 @@
 # python gpu_perf_vs_shape.py -l 1 2 4 6 9 12 16 20 25 32 -d 8 16 24 32 48 64 96 128 192 256 384 -b 1 2 5 10 20 40 60 100 --fp16 O0
 for l in 1 2 4 6 9 12 16 20 25 32
 do
-    for d in 8 16 24 32 48 64 96 128 192 256 384
+    for d in 8 16 32 64 128 256 512
     do
-        for b in 1 2 5 10 20 40 60 100
+        for b in 1 2 4 8 16 32 64 128
         do
-            for amp_mode in O1 O2
+            for amp_mode in O1 O2 O0
             do
-                python -W ignore gpu_perf_vs_shape.py -l $l -d $d -b $b --fp16 $amp_mode --reload
+                python -W ignore gpu_perf_vs_shape.py -l $l -d $d -b $b --fp16 $amp_mode --reload --tracking
             done
         done
     done
